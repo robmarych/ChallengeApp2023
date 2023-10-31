25d450d37d3e61f09a81d0bec7dd494b889d80db
robmarych <robmarych@gmail.com>
2023-10-31 21:09:42 +0100

    Day zadaanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name;
+char sex;
+int age;
+
+name = "Ewa";
+sex = 'K';
+age = 33;
+//Console.WriteLine(name +", lat " +  age );
+
+if (sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33 ");
+}
+else if ( sex !='K' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+
+
+
