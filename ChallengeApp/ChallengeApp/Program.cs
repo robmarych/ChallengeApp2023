fc61b6e50004cf38a6e8be877d2f0318c80d385e
robmarych <robmarych@gmail.com>
2023-11-01 21:40:37 +0100

    Day5 + zadanie

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,24 +1,60 @@
-﻿string name;
-char sex;
-int age;
+﻿//string name;
+//char sex;
+//int age;
 
-name = "Ewa";
-sex = 'K';
-age = 33;
-//Console.WriteLine(name +", lat " +  age );
+//name = "Ewa";
+//sex = 'K';
+//age = 33;
+////Console.WriteLine(name +", lat " +  age );
 
-if (sex == 'K' && age < 30)
+//if (sex == 'K' && age < 30)
+//{
+//    Console.WriteLine("Kobieta poniżej 30 lat");
+//}
+//else if (age == 33 && name == "Ewa")
+//{
+//    Console.WriteLine("Ewa, lat 33 ");
+//}
+//else if ( sex !='K' && age < 18)
+//{
+//    Console.WriteLine("Niepełnoletni Mężczyzna");
+//}
+
+
+//Policz ile razy wystąpiła liczba 
+
+
+int number = 4566;
+string numberAsString = number.ToString();
+char[] letter = numberAsString.ToCharArray();
+
+int[] counter = new int[10];
+
+int[] wynik = new int[10];
+
+List<int> list = new List<int>();
+
+foreach (char letterChar in letter)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+
+    for (int i = 0; i < 10; i++)
+    {
+
+        if (letterChar.ToString() == i.ToString())
+        {
+            counter[i] = counter[i] + 1;
+        }
+        else if (counter[i] == 0)
+        {
+            counter[i] = 0;
+        }
+        Console.WriteLine(counter[i]);
+    }
+
 }
-else if (age == 33 && name == "Ewa")
+
+
+for (int i = 0; i < wynik.Length; i++)
 {
-    Console.WriteLine("Ewa, lat 33 ");
+    Console.WriteLine(i + " wystąpiło " + counter[i] + " razy");
 }
-else if ( sex !='K' && age < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-
-
-
